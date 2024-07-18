ee97e136fb15330d8255411f62c5cd1aaf2283a1
sertemo <tejedor.moreno@gmail.com>
2024-07-18 22:05:07 +0200

    Añadida creación de carpeta logs en el workflow test

--- services/auth/src/main.py
+++ services/auth/src/main.py
@@ -1,4 +1,4 @@
-#   Copyright 2024 Carlos García, Afonso Teixeira, Carlos, Sergio Tejedor
+#   Copyright 2024 Carlos García, Afonso Teixeira, Sergio Tejedor
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -10,7 +10,7 @@
 #   distributed under the License is distributed on an "AS IS" BASIS,
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
-#   limitations under the License..
+#   limitations under the License.
 
 from contextlib import asynccontextmanager
 from datetime import datetime, timedelta, timezone
@@ -49,7 +49,7 @@ async def lifespan(app: FastAPI):
     logger.info(f"Base de datos '{USERSDB_NAME}' inicializada correctamente")
     yield
     os.remove(USERSDB_NAME)
-    logger.info("Base de datos '{USERSDB_NAME}' eliminada correctamente")
+    logger.info(f"Base de datos '{USERSDB_NAME}' eliminada correctamente")
 
 
 app = FastAPI(lifespan=lifespan)
